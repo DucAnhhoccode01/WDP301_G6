0670faafd85d03b5097728042fd05d2f1732f14a
hmtheboy154 <buingoc67@gmail.com>
2025-08-03 21:52:50 +0700

    Guard some pages under auth middleware
    New one like admin dashboard & coupon need to be guarded
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- back-end/src/routes/api/v1/dashboard.router.js
+++ back-end/src/routes/api/v1/dashboard.router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const DashboardController = require('../../../controllers/dashboard.controller');
+const AuthMiddleware = require('../../../middlewares/auth.middleware');
 
-router.get('/top-selling', DashboardController.getTopSellingProducts);
-router.get('/unsold', DashboardController.getUnsoldProducts);
-router.get('/expired', DashboardController.getExpiredProducts);
-router.get('/revenue', DashboardController.getTotalRevenue);
-router.get('/monthly-revenue', DashboardController.getMonthlyRevenue);
+router.get('/top-selling',AuthMiddleware.verifyToken, AuthMiddleware.verifyRole(['admin','sales manager']), DashboardController.getTopSellingProducts);
+router.get('/unsold',AuthMiddleware.verifyToken, AuthMiddleware.verifyRole(['admin','sales manager']), DashboardController.getUnsoldProducts);
+router.get('/expired',AuthMiddleware.verifyToken, AuthMiddleware.verifyRole(['admin','sales manager']), DashboardController.getExpiredProducts);
+router.get('/revenue',AuthMiddleware.verifyToken, AuthMiddleware.verifyRole(['admin','sales manager']), DashboardController.getTotalRevenue);
+router.get('/monthly-revenue',AuthMiddleware.verifyToken, AuthMiddleware.verifyRole(['admin','sales manager']), DashboardController.getMonthlyRevenue);
 
 module.exports = router;
